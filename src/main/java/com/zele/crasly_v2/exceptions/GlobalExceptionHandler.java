e597e3f035cb30a15829fe344392b5300c034416
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-20 16:15:09 +0100

    feat: add UserAlreadyExistsException

--- src/main/java/com/zele/crasly_v2/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zele/crasly_v2/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.zele.crasly_v2.exceptions;
 
+import com.zele.crasly_v2.exceptions.user.UserAlreadyExistsException;
 import com.zele.crasly_v2.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 }
---
+++ src/main/java/com/zele/crasly_v2/exceptions/user/UserAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package com.zele.crasly_v2.exceptions.user;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {super(message);}
+}
