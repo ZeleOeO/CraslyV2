54d025d7808b77521160108ab4a2c81d21e6c926
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-20 11:27:04 +0100

    chore: init

---
+++ src/test/java/com/zele/crasly_v2/CraslyV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zele.crasly_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraslyV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
