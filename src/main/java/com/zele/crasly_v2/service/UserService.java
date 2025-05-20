ab2a90a06a05f2a21adef0de379b0499cf3feaf7
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-20 15:46:13 +0100

    fix: change access modifiers in UserService

--- src/main/java/com/zele/crasly_v2/service/UserService.java
+++ src/main/java/com/zele/crasly_v2/service/UserService.java
@@ -17,14 +17,14 @@ public class UserService {
     private final UserMapper userMapper;
     private UserRepository userRepository;
 
-    List<UserViewDto> getAllUsers() {
+    public List<UserViewDto> getAllUsers() {
         return userRepository.findAll()
                 .stream()
                 .map(userMapper::toUserView)
                 .toList();
     }
 
-    ResponseEntity<UserViewDto> getUserById(Long id) {
+    public ResponseEntity<UserViewDto> getUserById(Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) throw new UserNotFoundException("User with id " + id + " not found");
         return ResponseEntity.status(HttpStatus.OK).body(userMapper.toUserView(user));
